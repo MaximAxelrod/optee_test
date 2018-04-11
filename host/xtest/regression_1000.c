d3f5cda12d62f94e29255742dce5300d371e3b8e
MaximAxelrod <maxim.axelrod@arm.com>
2018-04-11 10:51:39 +0300

    prints added with error

--- host/xtest/regression_1000.c
+++ host/xtest/regression_1000.c
@@ -271,6 +271,13 @@ static void xtest_tee_test_1001(ADBG_Case_t *c)
 	TEEC_Session session = { 0 };
 	uint32_t ret_orig;
 
+	Do_ADBG_Log("!!!!!!!!MAXIMA  1  !!!!!!!!!!!!!!!!!!!!!!");
+	Do_ADBG_BeginSubCase(cs->c, "!!!!!!!!MAXIMA  2  !!!!!!!!!!!!!!!!!!!!!!");
+	Do_ADBG_EndSubCase(cs->c, "!!!!!!!!MAXIMA  3  !!!!!!!!!!!!!!!!!!!!!!");
+	printf("!!!!!!!!MAXIMA  4  !!!!!!!!!!!!!!!!!!!!!!");
+
+EEEEEEEEEEEEEEEEEEEEEEEEEEEEeee
+
 	res = xtest_teec_open_session(&session, &pta_invoke_tests_ta_uuid, NULL,
 				      &ret_orig);
 	/*
